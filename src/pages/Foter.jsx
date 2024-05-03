41a137e4638e30f4104bebb876618694500cf78f
Hojiakbar Name <bekmirzayevhojiakbar306@example.com>
2024-05-03 15:02:21 +0500

    asasas

--- src/pages/Foter.jsx
+++ src/pages/Foter.jsx
@@ -1,7 +1,14 @@
 import foter from "../Css/Foter.module.css"
 function Foter() {
   return (
-    <div className={foter.fotercontainer}>Foter</div>
+    <div className={foter.orabtur}>
+      <div className={foter.fotercontainer}>
+        <div className={foter.fotertpa} >
+          <h1 className={foter.foteryozuv} >UNLOCK ID PERKS</h1>
+          <button className={foter.foterbutton}><h1 className={foter.fotteryouz}>SUBSCRIBE TO OUR NEWSLETTER</h1></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
